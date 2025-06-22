3d47d194e74fd623f392a614bfa774c32982f2cc
Akshay Varma <varmaakshay2020@gmail.com>
2025-06-22 23:30:01 +0530

    Connected frontend to live backend (socket.io)

--- src/components/ChatBox.jsx
+++ src/components/ChatBox.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3001");
+const socket = io("https://lone-town-server.onrender.com");
+
 
 const ChatBox = ({ onMilestone }) => {
   const [message, setMessage] = useState("");
